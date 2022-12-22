2beb39982fd592cd7f8e88b8de479da3beed2d19
ABEL0S0 <avilaospinabel@gmail.com>
2022-12-22 13:03:25 -0500

    xd

--- ProyectoBaseDeDatos_Abel-Avila/FrmAgregarEstudiantes.cs
+++ ProyectoBaseDeDatos_Abel-Avila/FrmAgregarEstudiantes.cs
@@ -20,20 +20,22 @@ namespace ProyectoBaseDeDatos_Abel_Avila
         private void btnGuadar_Click(object sender, EventArgs e)
         {
             try
-            { }
+            {
+                ProyectoBaseDeDatos_Abel_Avila.DATA_ACCess_OBJECT.Estudiante est =
+                    new ProyectoBaseDeDatos_Abel_Avila.DATA_ACCess_OBJECT.Estudiante();
+                est.Matricula = this.txtMatricula.Text;
+                est.Apellidos = this.txtApellidos.Text;
+                est.Nombres = this.txtNombres.Text;
+                est.Estatura = Int32.Parse(this.txtEstatura.Text);
+                est.FechaNacimiento = this.dtFechadenacimiento.Value;
+                est.Peso = float.Parse(this.txtPeso.Text);
+
+                ProyectoBaseDeDatos_Abel_Avila.DATA_ACCess_OBJECT.EstudianteDAO objEstudiante =
+                    new ProyectoBaseDeDatos_Abel_Avila.DATA_ACCess_OBJECT.EstudianteDAO();
+            }
             catch(Exception x)
             { MessageBox.Show(x.Message.ToString()); }
-            ProyectoBaseDeDatos_Abel_Avila.DATA_ACCess_OBJECT.Estudiante est =
-                new ProyectoBaseDeDatos_Abel_Avila.DATA_ACCess_OBJECT.Estudiante();
-            est.Matricula = this.txtMatricula.Text;
-            est.Apellidos = this.txtApellidos.Text;
-            est.Nombres = this.txtNombres.Text;
-            est.Estatura = Int32.Parse(this.txtEstatura.Text);
-            est.FechaNacimiento=this.dtFechadenacimiento.Value;
-            est.Peso = float.Parse(this.txtPeso.Text);
-
-            ProyectoBaseDeDatos_Abel_Avila.DATA_ACCess_OBJECT.EstudianteDAO objEstudiante =
-                new ProyectoBaseDeDatos_Abel_Avila.DATA_ACCess_OBJECT.EstudianteDAO();
+            
 
         }
     }
